2215a9c50cbbe3871e5f7efebc73938c71d62d28
MorganP0204 <106537983+MorganP0204@users.noreply.github.com>
2024-02-23 14:55:39 +0000

    Add API Integration for weather searching

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
+function updateWeather(response) {
+  console.log(response.data);
+  let temperature = response.data.temperature.current;
+  let currentTemperature = document.querySelector("#weather-app-temperature");
+  currentTemperature.innerHTML = Math.round(temperature);
+  let city = document.querySelector("#weather-app-city");
+  city.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "c60106tff65a6314cc5047o4d15bbc2a";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let city = document.querySelector("#weather-app-city");
-  city.innerHTML = searchInput.value;
-  // call the API
-  // search for the city we have
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
